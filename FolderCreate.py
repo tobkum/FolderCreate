261a38cc87109db835d471da19727bb57f3137cc
Tobias Kummer <tobiaskummer@googlemail.com>
2023-04-26 13:15:40 +0200

    rework

--- FolderCreate.py
+++ FolderCreate.py
@@ -30,7 +30,8 @@ dpg.create_context()
 
 
 def callback(_sender, app_data):
-    dpg.set_value(path_text, app_data["current_path"])
+    # Set the text field value to selected path
+    dpg.set_value("root_path", app_data["current_path"])
 
 
 def cancel_callback(_sender, _app_data):
@@ -38,23 +39,29 @@ def cancel_callback(_sender, _app_data):
 
 
 def create_folders():
+    # create the folder structure
     template = dpg.get_value("chosen_template")
     root_path = dpg.get_value("root_path")
-    with open(path.join(template_path, template), 'r') as stream:
+    with open(path.join(template_path, template), "r") as stream:
         data = safe_load(stream)
         dict_to_dir(data)
     for i in newpaths:
         try:
             makedirs(path.join(root_path, i))
         except Exception:
-            print('An error occured, please check if the directory is writable.')
-    dpg.configure_item("modal_id", show=True)
-    dpg.configure_item("message", default_value="Done")
+            dpg.configure_item("modal_id", show=True)
+            dpg.configure_item(
+                "message",
+                default_value="An error occured, please check if the directory is writable.",
+            )
+        else:
+            dpg.configure_item("modal_id", show=True)
+            dpg.configure_item("message", default_value="Done")
 
 
 with dpg.window(tag="Primary Window"):
     with dpg.group(horizontal=True) as directory_group:
-        path_text = dpg.add_input_text(default_value=default_path, tag="root_path")
+        dpg.add_input_text(default_value=default_path, tag="root_path")
         dpg.add_button(
             label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id")
         )
@@ -69,13 +76,16 @@ with dpg.window(tag="Primary Window"):
             default_path=default_path,
         )
 
-    dpg.add_combo(label="Select Template", items=templates, default_value=templates[0], tag="chosen_template")
-
-    dpg.add_button(
-        label="Create Folders", callback=create_folders, width=-1, height=50
+    dpg.add_combo(
+        label="Select Template",
+        items=templates,
+        default_value=templates[0],
+        tag="chosen_template",
     )
+
+    dpg.add_button(label="Create Folders", callback=create_folders, width=-1, height=50)
+    dpg.add_text("© 2023 Overmind Studios - Kummer & Gerhardt GbR")
     dpg.add_text("version 0.1")
-    dpg.add_text("© 2023 Overmind Studios")
 
     with dpg.window(
         label="Info",
@@ -84,13 +94,13 @@ with dpg.window(tag="Primary Window"):
         tag="modal_id",
         pos=(300, 200),
         width=200,
-        no_resize=True
+        no_resize=True,
     ):
         dpg.add_text("ERROR", tag="message")
         dpg.add_button(
             label="OK",
             callback=lambda: dpg.configure_item("modal_id", show=False),
-            width=-1
+            width=-1,
         )
 
 
