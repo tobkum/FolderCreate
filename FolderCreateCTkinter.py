7ef749553668f1353f2a63da198ef8ad4e2b5073
tobi <tobiaskummer@googlemail.com>
2025-03-26 17:52:07 +0100

    formatting

--- FolderCreateCTkinter.py
+++ FolderCreateCTkinter.py
@@ -44,7 +44,10 @@ def create_folders():
         try:
             os.makedirs(os.path.join(root_path, i), exist_ok=True)
         except Exception:
-            info_label.configure(text="An error occurred, please check if the directory is writable.", text_color="red")
+            info_label.configure(
+                text="An error occurred, please check if the directory is writable.",
+                text_color="red",
+            )
         else:
             info_label.configure(text="Done", text_color="green")
     info_window.deiconify()
@@ -58,7 +61,7 @@ def close_info_window():
 app = ctk.CTk()
 app.title("Overmind Studios FolderCreate " + version)
 app.geometry("600x400")
-app.resizable(False, False) # Prevent window resizing
+app.resizable(False, False)  # Prevent window resizing
 app.iconbitmap("assets/graphics/appicon.ico")
 app.grid_columnconfigure(0, weight=1)
 app.grid_rowconfigure(3, weight=1)
@@ -68,11 +71,15 @@ directory_frame = ctk.CTkFrame(app)
 directory_frame.grid(row=0, column=0, padx=20, pady=(20, 10), sticky="ew")
 directory_frame.grid_columnconfigure(0, weight=1)
 
-root_path_entry = ctk.CTkEntry(directory_frame, width=400, placeholder_text="Select Directory")
+root_path_entry = ctk.CTkEntry(
+    directory_frame, width=400, placeholder_text="Select Directory"
+)
 root_path_entry.insert(0, default_path)
 root_path_entry.grid(row=0, column=0, padx=(0, 10), pady=10, sticky="ew")
 
-choose_dir_button = ctk.CTkButton(directory_frame, text="Browse", command=choose_directory)
+choose_dir_button = ctk.CTkButton(
+    directory_frame, text="Browse", command=choose_directory
+)
 choose_dir_button.grid(row=0, column=1, padx=(0, 0), pady=10)
 
 # Template Selection Combobox
@@ -81,11 +88,19 @@ template_combobox.set(templates[0])
 template_combobox.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
 
 # Create Folders Button
-create_folders_button = ctk.CTkButton(app, text="Create Folders", command=create_folders, height=40, font=("Arial", 16, "bold"))
+create_folders_button = ctk.CTkButton(
+    app,
+    text="Create Folders",
+    command=create_folders,
+    height=40,
+    font=("Arial", 16, "bold"),
+)
 create_folders_button.grid(row=2, column=0, padx=20, pady=20, sticky="ew")
 
 # Copyright and Version Labels
-copyright_label = ctk.CTkLabel(app, text="© 2025 Overmind Studios - Kummer & Gerhardt GbR", font=("Arial", 10))
+copyright_label = ctk.CTkLabel(
+    app, text="© 2025 Overmind Studios - Kummer & Gerhardt GbR", font=("Arial", 10)
+)
 copyright_label.grid(row=4, column=0, pady=(10, 5))
 
 version_label = ctk.CTkLabel(app, text="version " + version, font=("Arial", 10))
